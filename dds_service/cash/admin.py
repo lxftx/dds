14b9d02b839696110223b1950646085f0400e71c
Oleg <olegtirex@mail.ru>
2025-04-24 18:16:27 +0500

    feat(admin): добавлены настройки отображения моделей в админке

--- dds_service/cash/admin.py
+++ dds_service/cash/admin.py
@@ -1,3 +1,28 @@
+from cash.models import OperationType, Status, Category, SubCategory, CashFlowRecord
+
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+@admin.register(OperationType)
+class OperationTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "operation_type")
+    search_fields = ("name",)
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category")
+    search_fields = ("name",)
+
+@admin.register(CashFlowRecord)
+class CashFlowRecordAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "status", "category", "subcategory", "operation_type")
